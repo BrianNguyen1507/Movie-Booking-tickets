dcbd83fd71a6537794526bf35c202529eb871d93
NGUYỄN PHI HIẾU <107748419+BrianNguyen1507@users.noreply.github.com>
2023-12-22 01:13:19 +0700

    add: detail page

--- lib/Views/movies_detail/movieDetail_screen.dart
+++ lib/Views/movies_detail/movieDetail_screen.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:movie_booking/Views/buyticket_screen/buy_tickets_screen.dart';
 import 'package:movie_booking/Views/index/index.dart';
-import 'package:movie_booking/Views/movies_detail/widgets/detail_widget.dart';
 import 'package:movie_booking/model/film/film.dart';
+import 'package:movie_booking/Views/movies_detail/widgets/expandableText_widget.dart';
+import 'package:movie_booking/services/converter.dart';
 
 class MovieDetailPage extends StatefulWidget {
-  final int filmId;
+  final Film film;
 
-  const MovieDetailPage({required this.filmId, Key? key}) : super(key: key);
+  const MovieDetailPage({super.key, required this.film});
 
   @override
   State<MovieDetailPage> createState() => _MovieDetailPageState();
@@ -21,49 +23,49 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
       decoration: const BoxDecoration(
         color: Colors.transparent,
       ),
-      child: Image.asset(
-        'assets/images/movie_banner.jpg',
-        fit: BoxFit.fill,
+      child: Image.memory(
+        toUint(widget.film.posters),
+        width: 150,
+        height: 180,
+        fit: BoxFit.cover,
       ),
     );
   }
 
   double total = 0.0;
 
-  Widget renderBooking(BuildContext context, int movieId) {
+  Widget renderBooking(BuildContext context) {
     return Container(
-      height: 100,
-      color: Colors.white,
-      padding: const EdgeInsets.all(10),
-      alignment: Alignment.center,
-      width: double.infinity,
-      child: SizedBox(
-        height: 50.0,
+        height: 100,
+        color: Colors.white,
+        padding: const EdgeInsets.all(10),
+        alignment: Alignment.center,
         width: double.infinity,
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: const Color.fromARGB(255, 41, 189, 58),
-            elevation: 0.0,
-          ),
-          onPressed: () {
-            _onRegisterPress(context);
-          },
-          child: const Text(
-            'BOOKING',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
+        child: SizedBox(
+          height: 50.0,
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 41, 189, 58),
+                elevation: 0.0),
+            onPressed: () {
+              _onRegisterPress(context);
+            },
+            child: const Text(
+              'BOOKING',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              ),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 
   Widget renderBody(BuildContext context) {
     return Expanded(
       child: Container(
-        color: Colors.transparent,
+        color: Colors.white,
         width: double.infinity,
         height: double.infinity,
         child: SingleChildScrollView(
@@ -71,7 +73,71 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               renderbanner(context),
-              const DetailWidget(),
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: Image.memory(
+                      toUint(widget.film.posters),
+                      width: 150,
+                      height: 180,
+                      fit: BoxFit.fitHeight,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Name: ${widget.film.title}',
+                          style: const TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                        ), // Replace 'title' with the actual property name
+                        Text(
+                          'Running time: ${widget.film.length}',
+                          style: const TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                        ), // Replace 'length' with the actual property name
+                        Text(
+                          'Date: ${widget.film.dateRelease}',
+                          style: const TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                        ), // Replace 'dateRelease' with the actual property name
+                        Text(
+                          'Actor: ${widget.film.actor}',
+                          style: const TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                        ), // Replace 'actor' with the actual property name
+                        Text(
+                          'Director: ${widget.film.director}',
+                          style: const TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const Gap(20),
+              const Text(
+                'Discribe:',
+                style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87),
+              ), //
+              ExpandableWidget(content: widget.film.describe),
             ],
           ),
         ),
@@ -93,45 +159,40 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => const IndexPage(
-                  title: '',
-                ),
-              ),
+                  builder: (context) => const IndexPage(
+                        title: '',
+                      )),
             );
           },
           icon: const Icon(Icons.arrow_back),
         ),
       ),
       body: Container(
-        constraints: const BoxConstraints.expand(),
-        child: Column(
-          children: [
-            Expanded(
-              child: SizedBox(
-                width: double.infinity,
-                height: double.infinity,
-                child: Column(
-                  children: [
-                    renderBody(context),
-                    renderBooking(context, 1), // Thay 1 bằng ID của phim
-                  ],
+          constraints: const BoxConstraints.expand(),
+          child: Column(
+            children: [
+              Expanded(
+                child: SizedBox(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: Column(
+                    children: [
+                      renderBody(context),
+                      renderBooking(context),
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
-        ),
-      ),
+              )
+            ],
+          )),
     );
   }
 }
 
-_onRegisterPress(
-  BuildContext context,
-) {
+_onRegisterPress(BuildContext context) {
   Navigator.push(
-    context,
-    MaterialPageRoute(
-      builder: (context) => SeatSelectionScreen(),
-    ),
-  );
+      context,
+      MaterialPageRoute(
+        builder: (context) => SeatSelectionScreen(),
+      ));
 }
--- lib/Views/movies_detail/widgets/expandableText_widget.dart
+++ lib/Views/movies_detail/widgets/expandableText_widget.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ExpandableWidget extends StatefulWidget {
-  const ExpandableWidget({super.key});
+  final String content;
+  const ExpandableWidget({required this.content, Key? key}) : super(key: key);
 
   @override
   State<ExpandableWidget> createState() => _ExpandableState();
@@ -9,16 +10,13 @@ class ExpandableWidget extends StatefulWidget {
 
 bool isExpanded = false;
 
-String content =
-    'In the heart of a bustling metropolis, where the vibrant pulse of urban life meets the tranquil embrace of greenery, a seamless fusion of contrasts unfolds. Towering skyscrapers reach for the heavens, casting shadows over bustling streets teeming with diverse faces and stories. Amidst the cacophony, hidden gems of serenity emerge—a pocket park adorned with blossoming flowers, a quiet café where time slows to a leisurely pace. The city breathes with a rhythm that harmonizes chaos and calm. This dynamic dance of elements creates a tapestry of experiences, where the energy of progress entwines with the timeless beauty of nature, shaping a unique and captivating urban narrative.';
-
 class _ExpandableState extends State<ExpandableWidget> {
   Widget expandableText(String content) {
     return Column(
       children: [
         Text(
-          isExpanded ? content : '${content.substring(0, 125)}...',
-          maxLines: isExpanded ? null : 3,
+          isExpanded ? content : '${content.substring(0, 90)}...',
+          maxLines: isExpanded ? null : 2,
         ),
         InkWell(
           onTap: () {
@@ -44,6 +42,6 @@ class _ExpandableState extends State<ExpandableWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return expandableText(content);
+    return expandableText(widget.content);
   }
 }
