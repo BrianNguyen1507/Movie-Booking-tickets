e98e6b94d97be08f834ac671b508384ad5a04bbd
brian nguyen <phihieu1507@gmail.com>
2024-03-16 01:12:22 +0700

    update: fix-detail-page

--- lib/Views/movies_detail/movieDetail_screen.dart
+++ lib/Views/movies_detail/movieDetail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
+import 'package:flutter/widgets.dart';
 import 'package:movie_booking/Views/buyticket_screen/buy_tickets_screen.dart';
+import 'package:movie_booking/Views/home_screen/widgets/AutoScrolling.dart';
 import 'package:movie_booking/Views/index/index.dart';
 import 'package:movie_booking/model/film/film.dart';
 import 'package:movie_booking/Views/movies_detail/widgets/expandableText_widget.dart';
@@ -17,49 +18,7 @@ class MovieDetailPage extends StatefulWidget {
 
 class _MovieDetailPageState extends State<MovieDetailPage> {
   Widget renderbanner(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 200,
-      decoration: const BoxDecoration(
-        color: Colors.transparent,
-      ),
-      child: Image.memory(
-        toUint(widget.film.posters),
-        width: 150,
-        height: 180,
-        fit: BoxFit.cover,
-      ),
-    );
-  }
-
-  double total = 0.0;
-
-  Widget renderBooking(BuildContext context) {
-    return Container(
-        height: 100,
-        color: Colors.white,
-        padding: const EdgeInsets.all(10),
-        alignment: Alignment.center,
-        width: double.infinity,
-        child: SizedBox(
-          height: 50.0,
-          width: double.infinity,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 41, 189, 58),
-                elevation: 0.0),
-            onPressed: () {
-              _onRegisterPress(context);
-            },
-            child: const Text(
-              'BOOKING',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ));
+    return const AutoScrollingBanner();
   }
 
   Widget renderBody(BuildContext context) {
@@ -75,12 +34,12 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
               renderbanner(context),
               Row(
                 children: [
-                  Container(
-                    padding: const EdgeInsets.all(20),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
                     child: Image.memory(
                       toUint(widget.film.posters),
-                      width: 150,
-                      height: 180,
+                      width: 130,
+                      height: 150,
                       fit: BoxFit.fitHeight,
                     ),
                   ),
@@ -90,59 +49,216 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          'Name: ${widget.film.title}',
+                          widget.film.title,
                           style: const TextStyle(
-                              fontSize: 17,
+                              fontSize: 20,
                               fontWeight: FontWeight.bold,
                               color: Colors.black87),
-                        ), // Replace 'title' with the actual property name
+                        ),
                         Text(
-                          'Running time: ${widget.film.length}',
+                          widget.film.categories
+                              .map((category) => category.name)
+                              .join(', '),
                           style: const TextStyle(
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black87),
-                        ), // Replace 'length' with the actual property name
-                        Text(
-                          'Date: ${widget.film.dateRelease}',
-                          style: const TextStyle(
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black87),
-                        ), // Replace 'dateRelease' with the actual property name
-                        Text(
-                          'Actor: ${widget.film.actor}',
-                          style: const TextStyle(
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black87),
-                        ), // Replace 'actor' with the actual property name
-                        Text(
-                          'Director: ${widget.film.director}',
-                          style: const TextStyle(
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black87),
+                              fontSize: 15,
+                              fontWeight: FontWeight.normal,
+                              color: Colors.black45),
                         ),
                       ],
                     ),
                   ),
                 ],
               ),
-              const Gap(20),
-              const Text(
-                'Discribe:',
-                style: TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black87),
-              ), //
-              ExpandableWidget(content: widget.film.describe),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                ),
+                margin: const EdgeInsets.only(top: 20.0),
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10, right: 10),
+                        child: Column(
+                          children: [
+                            const Text(
+                              'Date release',
+                              style: TextStyle(
+                                color: Colors.black45,
+                              ),
+                            ),
+                            Text(
+                              widget.film.dateRelease,
+                              style: const TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black87),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Container(
+                        height: 50,
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                      const SizedBox(width: 10),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10, right: 10),
+                        child: Column(
+                          children: [
+                            const Text(
+                              'Running time',
+                              style: TextStyle(
+                                color: Colors.black45,
+                              ),
+                            ),
+                            Text(
+                              widget.film.length,
+                              style: const TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black87),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Container(
+                        height: 50,
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                      const SizedBox(width: 10),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 10, right: 10),
+                        child: Column(
+                          children: [
+                            Text(
+                              'Language',
+                              style: TextStyle(
+                                color: Colors.black45,
+                              ),
+                            ),
+                            Text(
+                              'English',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black87),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Discribe',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                        ),
+                        const Divider(
+                          color: Colors.grey,
+                          thickness: 0.35,
+                        ),
+                        ExpandableWidget(content: widget.film.describe),
+                      ]),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                ),
+                height: 15,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Director & Actor',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87),
+                      ),
+                      const Divider(
+                        color: Colors.grey,
+                        thickness: 0.35,
+                      ),
+                      Text(
+                        ' ${widget.film.director} - ${widget.film.actor}',
+                        style: const TextStyle(
+                            fontSize: 15, color: Colors.black87),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  Widget renderBooking(BuildContext context) {
+    return Container(
+        decoration:  BoxDecoration(
+          color: Colors.grey[300],
+         
+        ),
+        height: 100,
+        padding: const EdgeInsets.all(10),
+        alignment: Alignment.center,
+        width: double.infinity,
+        child: SizedBox(
+          height: 50.0,
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shadowColor: Colors.black,
+              backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+              elevation: 15.0,
+            ),
+            onPressed: () {
+              _onRegisterPress(context);
+            },
+            child: const Text(
+              'BOOKING',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ));
   }
 
   @override
@@ -159,9 +275,10 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
             Navigator.push(
               context,
               MaterialPageRoute(
-                  builder: (context) => const IndexPage(
-                        title: '',
-                      )),
+                builder: (context) => const IndexPage(
+                  title: '',
+                ),
+              ),
             );
           },
           icon: const Icon(Icons.arrow_back),
