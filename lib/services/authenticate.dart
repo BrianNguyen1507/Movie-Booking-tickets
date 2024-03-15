a4c6395bb5f0e25729318d88496499e3abcd2a3d
brian nguyen <phihieu1507@gmail.com>
2024-03-15 19:49:19 +0700

    update : authen-login

--- lib/services/authenticate.dart
+++ lib/services/authenticate.dart
@@ -1,44 +1,39 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:movie_booking/model/users/User.dart';
 
 class AuthenticationService {
-  static Future<bool> authenticate(String username, String password) async {
+  static Future<bool> authenticate(User user) async {
     try {
-      const apiUrl = "http://192.168.1.3:8083/cinema/login";
+      const loginApi = "http://192.168.1.3:8083/cinema/login";
       final response = await http.post(
-        Uri.parse(apiUrl),
+        Uri.parse(loginApi),
         headers: {
           'Content-Type': 'application/json',
         },
         body: json.encode({
-          "user_name": username,
-          "password": password,
+          "user_name": user.account.username,
+          "password": user.account.password,
         }),
       );
 
       try {
         if (response.statusCode == 200) {
           final dynamic responseData = json.decode(response.body);
-          final String username = responseData['user_name'];
           final dynamic id = responseData['id'];
           final bool isAuthenticated = id != null;
           if (isAuthenticated) {
-            print('Authentication successful for user: $username');
             return true;
           } else {
-            print('Authentication failed for user: $username');
             return false;
           }
         } else {
-          print('Authentication Error: ${response.statusCode}');
           return false;
         }
       } catch (error) {
-        print('Exception: $error');
         return false;
       }
     } catch (error) {
-      print('Exception: $error');
       return false;
     }
   }
