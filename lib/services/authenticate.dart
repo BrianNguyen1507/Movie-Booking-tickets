779897ea544083196c5f71b8a4c1769e897a85b8
NGUYỄN PHI HIẾU <107748419+BrianNguyen1507@users.noreply.github.com>
2024-03-26 14:58:15 +0700

    fix: authen

--- lib/services/authenticate.dart
+++ lib/services/authenticate.dart
@@ -7,7 +7,7 @@ import 'package:movie_booking/services/ipconfig.dart';
 const storage = FlutterSecureStorage();
 
 class AuthenticationService {
-  static Future<bool> authenticate(User user) async {
+  static Future<Map<String, dynamic>> authenticate(User user) async {
     try {
       const loginApi = "http://$ip:8083/cinema/login";
       final response = await http.post(
@@ -26,30 +26,29 @@ class AuthenticationService {
           try {
             final dynamic responseData = json.decode(response.body);
             final dynamic id = responseData['id'];
-            // final dynamic token = responseData['token'];
-            // print('token: ${responseData['token']}');
+
             final bool isAuthenticated = id != null;
             if (isAuthenticated) {
-              // await storage.write(key: 'authToken', value: token);
-              return true;
+              // Return the authentication result along with the user ID
+              return {'authenticated': true, 'userId': id};
             } else {
-              return false;
+              return {'authenticated': false, 'userId': null};
             }
           } catch (e) {
             print('Error decoding JSON response: $e');
-            return false;
+            return {'authenticated': false, 'userId': null};
           }
         } else {
           print('Empty or null response body');
-          return false;
+          return {'authenticated': false, 'userId': null};
         }
       } else {
         print('HTTP Error: ${response.statusCode}');
-        return false;
+        return {'authenticated': false, 'userId': null};
       }
     } catch (error) {
       print('Error in authentication request: $error');
-      return false;
+      return {'authenticated': false, 'userId': null};
     }
   }
 }
