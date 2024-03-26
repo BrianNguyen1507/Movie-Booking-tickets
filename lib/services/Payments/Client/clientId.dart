c6f277772a593c7c00224a076c8d3c0ce1a44a55
NGUYỄN PHI HIẾU <107748419+BrianNguyen1507@users.noreply.github.com>
2024-03-26 21:27:34 +0700

    add: Client id & key

--- lib/services/Payments/Client/clientId.dart
+++ lib/services/Payments/Client/clientId.dart
@@ -1,4 +1,4 @@
 String clientid =
-    "none";
+    "AaMaq-xtLgehPl43CWE5Uqm97ntvKUZCwxqKIw9XAow-Cvc0c3GkRVRP9ff0gdad4uKiDiKHzppEjq5W";
 String secretKey =
-    "none";
+    "EHQeYD85PppTFkIZhsCzjXDu4oBF3ZW42EhRomD4dn2jbPJC8Cw2pxtViLwGj_iQxKSrvbyySUROBAxM";
