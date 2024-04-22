15152610ce280f34c8f2db629d9cae66a8e832aa
lepodo123 <116814338+lepodo123@users.noreply.github.com>
2024-04-22 09:17:47 +0700

    fix get all salary by month

--- src/main/java/com/cinema/converter/TimeSheetsConverter.java
+++ src/main/java/com/cinema/converter/TimeSheetsConverter.java
@@ -5,10 +5,7 @@ import com.cinema.dto.reponse.SalaryTotalResponse;
 import com.cinema.dto.reponse.TimeSheetsResponse;
 import com.cinema.entity.TimeSheetsEntity;
 import com.cinema.util.DateFormatter;
-import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Calendar;
@@ -18,15 +15,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TimeSheetsConverter {
     static final String STATUS_APPROVED = "approved";
-    public TimeSheetsResponse toTimeSheetsResponse(TimeSheetsEntity timeSheetsEntity,long id, String name){
+
+    public TimeSheetsResponse toTimeSheetsResponse(TimeSheetsEntity timeSheetsEntity, long id, String name) {
         TimeSheetsResponse response = new TimeSheetsResponse();
 
         response.setId(timeSheetsEntity.getId());
         response.setTimeIn(DateFormatter.parseTime(timeSheetsEntity.getTimeIn()));
-        if(timeSheetsEntity.getTimeOut()==null){
-            response.setTimeOut("Not yet check out");
-        }
-        else{
+        if (timeSheetsEntity.getTimeOut() == null) {
+            response.setTimeOut("Not yet");
+        } else {
             response.setTimeOut(DateFormatter.parseTime(timeSheetsEntity.getTimeOut()));
         }
         response.setStatus(timeSheetsEntity.getStatus());
@@ -39,26 +36,27 @@ public class TimeSheetsConverter {
         return response;
     }
 
-    public SalaryTotalResponse toSalaryTotalResponse(List<TimeSheetsEntity> timeSheetsEntities){
+    public SalaryTotalResponse toSalaryTotalResponse(List<TimeSheetsEntity> timeSheetsEntities) {
         SalaryTotalResponse response = new SalaryTotalResponse();
 
         response.setId(timeSheetsEntities.getFirst().getEmployee().getId());
         response.setSalary(timeSheetsEntities.getFirst().getSalary());
         Calendar timeIn = Calendar.getInstance();
-        int totalHour =0;
-        for (TimeSheetsEntity entity : timeSheetsEntities){
+        int totalHour = 0;
+        for (TimeSheetsEntity entity : timeSheetsEntities) {
             timeIn.setTime(entity.getTimeIn());
             Calendar timeOut = Calendar.getInstance();
-            timeOut.setTime(entity.getTimeOut());
-            int hourDay = (timeOut.get(Calendar.HOUR_OF_DAY)-timeIn.get(Calendar.HOUR_OF_DAY));
-            if(entity.getStatus().equals(STATUS_APPROVED)){
-                totalHour += Math.min(hourDay, 8);
+            if (entity.getTimeOut()!=null){
+                timeOut.setTime(entity.getTimeOut());
+                int hourDay = (timeOut.get(Calendar.HOUR_OF_DAY) - timeIn.get(Calendar.HOUR_OF_DAY));
+                if (entity.getStatus().equals(STATUS_APPROVED)) {
+                    totalHour += Math.min(hourDay, 8);
+                }
             }
 
         }
         response.setHourWorking(totalHour);
         response.setName(timeSheetsEntities.getFirst().getEmployee().getAccount().getCustomer().getName());
-
         return response;
     }
 }
